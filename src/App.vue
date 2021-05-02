9b880b5101bdcfdc582db3a3d2e991248e0deb81
Holden <holden.madagame@gmail.com>
2021-05-02 23:31:26 +0200

    Basic styling with margins and padding, borders styled

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,25 @@
 <template>
   <div id="app">
-    <Header />
-    <img alt="Vue logo" src="./assets/logo.png" />
+    <!-- <Header /> -->
+    <img class="banner" alt="Same Game Twin Banner" src="./assets/10.png" />
+    
     <Loader />
-    <Form msg="When were you born?" />
-    <div>
-      <div>Game Name: {{this.games[0].name}}</div>
-      <div>Platform: </div>
-      <div>Release Date: {{convertUnixDate(this.games[0].first_release_date)}}</div>
+    
+    <div class="gradient-border">
+      <div class="component">
+        <Form msg="When were you born?" />
+      </div>
     </div>
     
-    <div>Footer component</div>
+    <div class="gradient-border">
+      <div class="component">
+        <div>Game Name: {{this.games[0].name}}</div>
+        <div>Platform: </div>
+        <div>Release Date: {{convertToHumanDate(this.games[0].first_release_date)}}</div>
+      </div>
+    </div>
+    
+    <Footer />
     <div></div>
   </div>
 </template>
@@ -18,7 +27,7 @@
 <script>
 // Imports
 import Form from "./components/Form.vue";
-import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 import Loader from './components/Loader.vue';
 import { getAccessToken, getGames } from "./api_requests";
 
@@ -26,7 +35,7 @@ export default {
   name: "App",
   components: {
     Form,
-    Header,
+    Footer,
     Loader,
   },
 
@@ -59,12 +68,37 @@ export default {
 </script>
 
 <style>
+
+.gradient-border {
+  width: 90%;
+  margin: 1rem;
+  padding: 5px;
+  background: rgb(28,203,227);
+  background: -moz-linear-gradient(99deg, rgba(28,203,227,1) 0%, rgba(125,45,253,1) 100%);
+  background: -webkit-linear-gradient(99deg, rgba(28,203,227,1) 0%, rgba(125,45,253,1) 100%);
+  background: linear-gradient(99deg, rgba(28,203,227,1) 0%, rgba(125,45,253,1) 100%);
+  filter: progid:DXImageTransform.Microsoft.gradient(startColorstr="#1ccbe3",endColorstr="#7d2dfd",GradientType=1); 
+}
+
+.component {
+  background: white;
+  color: black;
+  padding: 2rem;
+}
+
 #app {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+}
+.banner {
+  width: 90%;
+  max-width: 800px;
 }
 </style>
--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,24 @@
-<template lang=""><div> Footer </div></template>
+<template lang="">
+    <div class="footer">
+        <span class="footer-item">About</span>
+        <span class="footer-item">Github</span>
+        <span class="footer-item">LinkedIn</span>
+    </div>
+</template>
 <script>
-export default {};
+export default {
+    name: "Footer",
+};
 </script>
-<style lang=""></style>
+
+<style>
+.footer {
+    display: flex;
+    flex-direction: row;
+    justify-content: center;
+}
+
+.footer-item {
+    padding: 1rem 1rem;
+}
+</style>
