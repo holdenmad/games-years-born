41580b750b05dfac226a3ea4e888bf84dd09911b
Holden <holden.madagame@gmail.com>
2021-05-02 05:48:05 +0200

    Write date conversion methods (Unix > Human and Human > Unix)

--- src/App.vue
+++ src/App.vue
@@ -4,6 +4,12 @@
     <img alt="Vue logo" src="./assets/logo.png" />
     <Loader />
     <Form msg="When were you born?" />
+    <div>
+      <div>Game Name: {{this.games[0].name}}</div>
+      <div>Platform: </div>
+      <div>Release Date: {{convertUnixDate(this.games[0].first_release_date)}}</div>
+    </div>
+    
     <div>Footer component</div>
     <div></div>
   </div>
@@ -28,6 +34,17 @@ export default {
     return {
       games: null,
       accessToken: '',
+    }
+  },
+
+  methods: {
+    convertToHumanDate(unixDate) {
+      let date = new Date(unixDate);
+      return date.toLocaleDateString()
+    },
+
+    convertToUnixDate(dateString) {
+      return Date.parse(dateString)/1000;
     }
   },
 
